5231d28c3ece4477ecd869a9bbe376b1287d1a7f
Jeff Zinger <jeff.zinger@easyjet.com>
2025-10-24 10:17:12 +0200

    Remove blog

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,5 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
-	type: 'content',
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-		tags: z.array(z.string()).optional(),
-	}),
-});
-
 const projects = defineCollection({
 	type: 'content',
 	schema: z.object({
@@ -39,7 +27,6 @@ const prompts = defineCollection({
 });
 
 export const collections = {
-	blog,
 	projects,
 	prompts,
 };
